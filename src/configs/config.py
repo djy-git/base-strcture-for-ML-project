0ba8c7126d9a7c9b0b11ba9a31966b1eb87841ce
djy-git <djyoon0223@gmail.com>
2020-09-30 17:21:42 +0900

    Modify ROOT_DIR_PATH in config.py

--- src/configs/config.py
+++ src/configs/config.py
@@ -3,8 +3,7 @@ from configs.config_user import *
 
 
 ### Input, Output, Log directory
-ROOT_DIR_PATH   = dirname(os.getcwd())
+ROOT_DIR_PATH   = dirname(dirname(dirname(__file__)))
 INPUT_DIR_PATH  = join(ROOT_DIR_PATH, "input")
 OUTPUT_DIR_PATH = join(ROOT_DIR_PATH, "output")
 LOG_DIR_PATH    = join(ROOT_DIR_PATH, "log")
-
